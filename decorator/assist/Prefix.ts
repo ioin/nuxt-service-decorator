a82c47d08fb4bcb28b21ab5d71e9403b327f9ad7
donghui <donghui@qiruo.com>
2021-03-10 20:07:48 +0800

    rename

--- decorator/assist/Prefix.ts
+++ decorator/assist/Prefix.ts
@@ -1,3 +1,8 @@
+/**
+ * route prefix for @Get @Post @Arbitrary method
+ * @param prefix 
+ * @returns 
+ */
 export default function ServiceRoutePrefix(prefix: string) {
     return function (target: any) {
         target.prototype.$prefix = prefix
